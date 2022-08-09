0c6365ab16fe40fc9ed130c47c33bb911a7b3ca4
mfarzamalam <mfarzamalam@gmail.com>
2022-08-09 22:42:59 +0500

    fetch the data from stripe for subscription payment and dump it into local db

--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 import datetime
-from stripe_subscription.models import UserSubscription
+from stripe_subscription.models import ProductStripeMapping, UserSubscription
 from stripe_user.models import UserStripeMapping
 from .models import Product
 import stripe
@@ -15,19 +15,49 @@ def home_page(request):
 
 
 def user_dashborad(request):
+    #  cronjob start
+    user_stripe_id = UserStripeMapping.objects.filter(i_user=request.user).first().stripe_id
+    print("user_stripe_id:", user_stripe_id)
+    # stripe_customer_qs = stripe.Customer.retrieve("cus_MBSycfc3VRAkH8")
+    # print("stripe_customer_qs:", stripe_customer_qs)
+    
+    stripe_sub_qs = stripe.Subscription.list(customer=user_stripe_id)
+    print("stripe_sub_qs:", len(stripe_sub_qs))
+
+    for stripe_obj in stripe_sub_qs:
+        print("stripe_obj:", stripe_obj)
+        stripe_obj_id = stripe_obj.id
+        user_sub_obj = UserSubscription.objects.filter(subscription_id=stripe_obj_id).first()
+        if not user_sub_obj:
+            stripe_product_id = stripe_obj.plan.product
+            date_created = stripe_obj.get('current_period_start')
+            date_ended = stripe_obj.get('current_period_end')
+            subscription_start_date = datetime.datetime.fromtimestamp(float(date_created))
+            subscription_end_date = datetime.datetime.fromtimestamp(float(date_ended))
+            try:
+                product_obj = ProductStripeMapping.objects.get(product_stripe_id=stripe_product_id).i_product
+            except:
+                break
+            
+            # print("stripe_product_id:", stripe_product_id)
+            # print("date_created:", date_created)
+            # print("date_ended:", date_ended)
+            # print("subscription_start_date:", subscription_start_date)
+            # print("subscription_end_date:", subscription_end_date)
+            # print("product_obj:", product_obj)
+            UserSubscription.objects.create(
+                i_customer = request.user,
+                i_product = product_obj,
+                subscription_start_date = subscription_start_date,
+                subscription_end_date = subscription_end_date,
+                subscription_status = 'active',
+                subscription_id = stripe_obj_id
+            )
+            print("obj is created")
+            print()
+    #  cronjob end
+
     data = UserSubscription.objects.filter(i_customer=request.user)
-    # stripe_user_id = UserStripeMapping.objects.get(i_user=request.user).stripe_id
-    # stripe_user_obj = stripe.Subscription.list(customer=stripe_user_id)
-    # data = list()
-    # for stripe_data in stripe_user_obj:
-    #     print(stripe_data)
-    #     data.append(
-    #         {
-    #             'current_period_start': datetime.fromtimestamp(float(stripe_data.current_period_start)),
-    #             'current_period_end': datetime.fromtimestamp(float(stripe_data.current_period_end)),
-    #         }
-    #     )
-    #     print()
     context = {'data_dict': data}
     return render(request, 'product/user_dashboard.html', context)
 
@@ -38,23 +68,8 @@ def view_product(request):
     print("current_date:", current_date)
     product_qs = list(Product.objects.all().values())
     for product_obj in product_qs:
-        # prod_subs = UserProductSubscription.objects.filter(customer=request.user,
-        # product_id=product_obj.get('id'), status='active', )
-        # if prod_subs:
-        #     start_date = prod_subs.subscription_start.strftime('%y-%m-%d')
-        #     end_date = prod_subs.subscription_end.strftime('%y-%m-%d')
-        #     print("start_date:", start_date)
-        #     print("end_date:", end_date)
-        #     if start_date <= current_date and end_date >= current_date:
-        #         product_obj.update({'button': 'already'})
-        #     else:
-        #         product_obj.update({'button': 'buy'})
-        # else:
-        #     product_obj.update({'button': 'buy'})
         product_obj.update({'button': 'buy'})
-        
         print("button:", product_obj.get('button'))
-        print()
 
     context = {'products': product_qs}
     return render(request, 'product/view_product.html', context)
